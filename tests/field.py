c878a2e2345b6d00475a4cc28b7344a63e51f0bc
Lauren Ko <lauren.ko@unt.edu>
2016-01-11 13:12:12 -0600

    Digi style changes to tests/field.py

--- tests/field.py
+++ tests/field.py
@@ -1,12 +1,12 @@
 import unittest
+
 from pyuntl.untl_structure import PYUNTL_DISPATCH, UNTLStructureException
-from tests import UNNORMALIZED_DICT, NORMALIZED_DICT, UNNORMALIZED_UNTLBS, \
-    UNNORMALIZED_LCSH, NORMALIZED_UNTLBS, NORMALIZED_LCSH
 from pyuntl.util import untldict_normalizer, normalize_UNTL, normalize_LCSH
+from tests import (UNNORMALIZED_DICT, NORMALIZED_DICT, UNNORMALIZED_UNTLBS,
+                   UNNORMALIZED_LCSH, NORMALIZED_UNTLBS, NORMALIZED_LCSH)
 
 
 class FieldTest(unittest.TestCase):
-
     def setUp(self):
         self.field = PYUNTL_DISPATCH['title'](content=None)
 
@@ -44,19 +44,19 @@ class FieldTest(unittest.TestCase):
 
     def testLCSHFieldNormalization(self, lcsh_content=UNNORMALIZED_LCSH,
                                    normalized_content=NORMALIZED_LCSH):
-        """Tests normalizing an LCSH string"""
+        """Test normalizing an LCSH string."""
         norm = normalize_LCSH(lcsh_content)
         self.assertEqual(norm, normalized_content)
 
     def testUNTLBSFieldNormalization(self, untlbs_content=UNNORMALIZED_UNTLBS,
                                      normalized_content=NORMALIZED_UNTLBS):
-        """Tests normalizing a UNTL-BS string"""
+        """Test normalizing a UNTL-BS string."""
         norm = normalize_UNTL(untlbs_content)
         self.assertEqual(norm, normalized_content)
 
     def testSingleNormalization(self, untl_dict=UNNORMALIZED_DICT,
                                 normalized_dict=NORMALIZED_DICT):
-        """Test normalizing LCSH untl data"""
+        """Test normalizing LCSH UNTL data."""
         normalize_required = {
             'subject': ['LCSH'],
             }
@@ -65,7 +65,7 @@ class FieldTest(unittest.TestCase):
 
     def testAllNormalizations(self, untl_dict=UNNORMALIZED_DICT,
                               normalized_dict=NORMALIZED_DICT):
-        """Tests all the normalizations for untl"""
+        """Test all the normalizations for UNTL."""
         normalize_required = {
             'subject': ['LCSH', 'UNTL-BS'],
             }
