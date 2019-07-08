3ace1016231a0141c1e8798960f81ab14f86e3e4
Lauren Ko <lauren.ko@unt.edu>
2019-07-08 18:42:48 -0500

    Add more metadata_generator tests

--- tests/metadata_generator_test.py
+++ tests/metadata_generator_test.py
@@ -2,6 +2,7 @@ import os
 from unittest.mock import patch
 
 import pytest
+from lxml.etree import Element, tostring
 
 from pyuntl import (metadata_generator as mg, untl_structure as us,
                     etd_ms_structure as es)
@@ -75,3 +76,70 @@ def test_pydict2xml_raises_MetadataGeneratorException(mock_open):
     msg = 'Failed to create an XML file. Filename: {}'.format(path)
     assert err.value.args[0] == msg
     mock_open.assert_called_once_with(path, 'w', encoding='utf-8')
+
+
+def test_pydict2xmlstring_no_root_namespace_no_nsmap():
+    metadata_dict = {'title': [{'qualifier': 'serialtitle',
+                                'content': 'A Title'}],
+                     'creator': [{'qualifier': 'aut',
+                                  'content': {'name': 'Noway, José'}}]}
+    xml = mg.pydict2xmlstring(metadata_dict)
+    assert xml.decode('utf-8') == ('<?xml version="1.0" encoding="UTF-8"?>\n'
+                                   '<metadata>\n'
+                                   '  <title qualifier="serialtitle">A Title</title>\n'
+                                   '  <creator qualifier="aut">\n'
+                                   '    <name>Noway, José</name>\n'
+                                   '  </creator>\n'
+                                   '</metadata>\n')
+
+
+def test_pydict2xmlstring_root_namespace_attributes_nsmap():
+    metadata_dict = {'title': [{'qualifier': 'officialtitle',
+                                'content': 'A Dissertation'}],
+                     'degree': [{'content': {'grantor': 'UNT'}}],
+                     'contributor': [{'role': 'chair', 'content': 'Case, J.'}],
+                     'subject': [{'scheme': 'LC',
+                                  'content': 'dog'}]}
+    xml = mg.pydict2xmlstring(metadata_dict,
+                              root_attributes={'schemaLocation': 'http://example.org/schema'},
+                              root_namespace='{http://purl.org/dc/elements/1.1/}',
+                              namespace_map={'dc': 'http://purl.org/dc/elements/1.1/'})
+    assert xml.decode('utf-8') == ('<?xml version="1.0" encoding="UTF-8"?>\n'
+                                   '<dc:metadata xmlns:dc="http://purl.org/dc/elements/1.1/"'
+                                   ' schemaLocation="http://example.org/schema">\n'
+                                   '  <title qualifier="officialtitle">A Dissertation</title>\n'
+                                   '  <contributor role="chair">Case, J.</contributor>\n'
+                                   '  <subject scheme="LC">dog</subject>\n'
+                                   '  <degree>\n'
+                                   '    <grantor>UNT</grantor>\n'
+                                   '  </degree>\n'
+                                   '</dc:metadata>\n')
+
+
+def test_pydict2xmlstring_no_root_namespace_nsmap():
+    metadata_dict = {'title': [{'qualifier': 'serialtitle',
+                                'content': 'A Title'}],
+                     'creator': [{'qualifier': 'aut',
+                                  'content': {'name': 'Noway, José'}}]}
+    xml = mg.pydict2xmlstring(metadata_dict,
+                              namespace_map={'dc': 'http://purl.org/dc/elements/1.1/'})
+    assert xml.decode('utf-8') == ('<?xml version="1.0" encoding="UTF-8"?>\n'
+                                   '<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">\n'
+                                   '  <title qualifier="serialtitle">A Title</title>\n'
+                                   '  <creator qualifier="aut">\n'
+                                   '    <name>Noway, José</name>\n'
+                                   '  </creator>\n'
+                                   '</metadata>\n')
+
+
+def test_create_dict_subelement():
+    root = Element('root', nsmap={'animal': 'http://example.com/animal'})
+    mg.create_dict_subelement(root,
+                              'dog',
+                              'Harold',
+                              attribs={'role': 'pet'},
+                              namespace='{http://example.com/animal}')
+    xml = tostring(root, pretty_print=True)
+    assert xml.decode('utf-8') == ('<root xmlns:animal="http://example.com/animal">\n'
+                                   '  <animal:dog role="pet">Harold</animal:dog>\n'
+                                   '</root>\n')
